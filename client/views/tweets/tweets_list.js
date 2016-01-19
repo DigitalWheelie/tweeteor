3d5ce981ca16b42a97b4156426b13cc5f65cbdcc
Ryan Glover <me@ryanglover.net>
2016-01-19 17:28:51 -0600

    get a janky animation working with p5

--- client/views/tweets/tweets_list.js
+++ client/views/tweets/tweets_list.js
@@ -7,34 +7,95 @@ Template.tweetsList.onRendered( function() {
 
   Tracker.autorun( function( computation ) {
 
-    var tweets = Tweets.find().fetch();
+    var tweets = Tweets.find().fetch(),
+        system;
 
     if ( tweets.length === 50 ) {
       new p5( function( sketch ) {
 
+        var ParticleSystem = function(position) {
+          // Figure out how to identify each circle's corresponding diameter.
+          this.origin = position.copy();
+          this.particles = [];
+        };
+
+        ParticleSystem.prototype.addParticle = function() {
+          this.particles.push(new Particle(this.origin));
+        };
+
+        ParticleSystem.prototype.run = function( tweetLength ) {
+          for (var i = this.particles.length-1; i >= 0; i--) {
+            var particle = this.particles[i];
+            particle.run( tweetLength );
+
+            if ( particle.isDead() ) {
+              this.particles.splice(i, 1);
+            }
+          }
+        };
+
+        // A simple Particle class
+        var Particle = function(position) {
+          this.acceleration = sketch.createVector(0, 0.05);
+          this.velocity = sketch.createVector( sketch.random(-1, 1), sketch.random(-1, 0));
+          this.position = position.copy();
+          this.lifespan = 255.0;
+        };
+
+        Particle.prototype.run = function( tweetLength ) {
+          this.update();
+          this.display( tweetLength );
+        };
+
+        // Method to update position
+        Particle.prototype.update = function(){
+          this.velocity.add(this.acceleration);
+          this.position.add(this.velocity);
+          this.lifespan -= 2;
+        };
+
+        // Method to display
+        Particle.prototype.display = function( diameter ) {
+          sketch.stroke(200, this.lifespan);
+          sketch.strokeWeight(2);
+          sketch.fill(127, this.lifespan);
+          sketch.ellipse(this.position.x, this.position.y, diameter, diameter );
+        };
+
+        // Is the particle still useful?
+        Particle.prototype.isDead = function(){
+          if (this.lifespan < 0) {
+            return true;
+          } else {
+            return false;
+          }
+        };
+
+        var tweet;
+
         sketch.setup = function() {
           sketch.createCanvas(720, 400);
+          tweet = 0;
+          // offset x-axis, y-axis of the entire particle drawing
+          system = new ParticleSystem( sketch.createVector( sketch.width / 2, sketch.height / 2 ) );
         };
 
         sketch.draw = function() {
-          var count = 0;
+          tweet++;
 
-          for ( var i = 0; i < tweets.length; i++ ) {
-            // x, y, width, height
-            var diameter = tweets[i].tweet.length,
-                offset   = 100;
-
-            sketch.ellipse( offset + i, offset + i, diameter, diameter);
-            sketch.noFill();
-            count++;
+          if ( tweet === 50 ) {
+            tweet = 0;
           }
 
-          if ( count === 50 ) {
-            sketch.noLoop();
-          }
+          var tweetLength = tweets[ tweet ].tweet.length;
+
+          sketch.background( 51 );
+          system.addParticle();
+          system.run( tweetLength );
         };
 
       }, "tweets-sketch" );
+
       computation.stop();
     }
   });
